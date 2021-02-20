63069bd3edd837dac4a6934f3bef90d9aab3841e
Mack Raicevic <makiraicevic@gmail.com>
2021-02-20 02:48:48 -0800

    update

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 Events API for the client project
 reference https://www.youtube.com/watch?v=qj2oDkvc4dQ&list=PLZlA0Gpn_vH8jbFkBjOuFjhxANC63OmXM&index=5&ab_channel=WebDevSimplified
 
+### Heroku Deployment
+https://events-api-megamixbend.herokuapp.com/ 
+
 ### Tasks
 [] - Create User Input
 [] - User insert data into Event
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+// If we are in local load env
+if( process.env.NODE_ENV !== 'production'){
+  require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+
+const indexRouter = require('./routes/index');
+
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -8,6 +16,22 @@ app.set('layout', 'layouts/layout');
 
 app.use(expressLayouts);
 app.use(express.static('public'));
+
+
+// MongoDB
+const mongoose = require('mongoose');
+// Connection to DB
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// check the DB connection
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose'));
+
+
+// use default route for routes/index.js
+app.use('/', indexRouter);
+
 
 //liste on port
 app.listen(process.env.PORT || 4000);
